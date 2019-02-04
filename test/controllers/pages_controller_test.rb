7a9055d218665cc55e78ac703f60d9bce5a401c8
Edbert Ian M. Abundabar <edbertianabundabar@gmail.com>
2019-02-04 14:37:06 +0800

    Make User model

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -3,21 +3,14 @@ require 'test_helper'
 class PagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do
-    get root_url
-    assert_response :success
-    assert "title", "Home | Site App"
-  end
-
-  test "should get home" do
-    get pages_home_url
+    get root_path
     assert_response :success
     assert "title", "Home | Site App"
   end
 
   test "should get about" do
-    get pages_about_url
+    get about_path
     assert_response :success
     assert "title", "About | Site App"
   end
-
 end
