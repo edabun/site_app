185714ab68eeda94708f2f7fa6f1aa4d4d2a1c50
Edbert Ian M. Abundabar <edbertianabundabar@gmai.com>
2019-02-04 10:15:42 +0800

    Added pages

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+end
