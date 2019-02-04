c742ca5064da7545ba33412cccd1d0acb882fc5b
Edbert Ian M. Abundabar <edbertianabundabar@gmai.com>
2019-02-04 10:31:36 +0800

    Added full_title helper

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,16 +5,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+    assert "title", "Home | Site App"
   end
 
   test "should get home" do
     get pages_home_url
     assert_response :success
+    assert "title", "Home | Site App"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert "title", "About | Site App"
   end
 
 end
